04b7db1463146e8c73b1b7e4eac7a8873c831668
giulianosrebuli <giuliano.srebuli@gmail.com>
2024-05-17 11:55:40 -0300

    Add battery read data

--- src/sensor.ino
+++ src/sensor.ino
@@ -2,7 +2,18 @@
 #include <TinyWireM.h>
 #include <SI7021.h>
 
+/**
+ * ATTINY PINS   _______
+ * 1 -  BAT LVL |       |    VCC     - 8
+ * 2 -  OPT RX  |       |  TEMP SCL  - 7
+ * 3 -  OPT TX  |       |  RF OUT    - 6
+ * 4 -   GND    |_______|  TEMP SDA  - 5
+ * 
+**/
+
 #define SERIAL_DEBUG 0
+#define BAT_LVL_PIN 0
+
 
 #if SERIAL_DEBUG
   #include <SoftwareSerial.h>
@@ -18,9 +29,11 @@ void setup() {
     sensor.begin();
 
 #if SERIAL_DEBUG    
-    softSerial.begin(9600);
+    softSerial.begin(9600); // ONLY WORKS WITH THIS BAUD RATE
     softSerial.println("--- ATtiny85 SI7021 Temp & Hum ---\n");
 #endif
+
+    pinMode(BAT_LVL_PIN, INPUT);
 }
 
 void loop() {
@@ -29,6 +42,7 @@ void loop() {
 
     float temp = ((float)data.celsiusHundredths)/100;
     float hum = ((float)data.humidityBasisPoints)/100;
+    int bat = analogRead(BAT_LVL_PIN);
 
 #if SERIAL_DEBUG    
     softSerial.print("Temperature: ");
@@ -38,6 +52,10 @@ void loop() {
     softSerial.print("Humidity: ");
     softSerial.print(hum,2);
     softSerial.println(" %\n");
+
+    softSerial.print("Battery: ");
+    softSerial.print(bat);
+    softSerial.println(" ADC\n");
 #endif
 
     snore(SLEEP_TIME_MS); // deep-sleep like - measured 5uA of consumption
